7027a87c349baa0cecd1ab82297f97c6ec0d5aa9
Awoodz <a.bois77@gmail.com>
2020-07-22 18:07:27 +0200

    HTML/CSS template modification

--- pur_beurre_project/home/views.py
+++ pur_beurre_project/home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def index(request):
 
-    template = loader.get_template('home/index.html')
+    template = loader.get_template('home/base.html')
     return HttpResponse(template.render(request=request))

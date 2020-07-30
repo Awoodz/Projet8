d3348313ebd6caba578d5d5439235b23ad68536c
Awoodz <a.bois77@gmail.com>
2020-07-30 12:35:49 +0200

    change settings

--- pur_beurre_project/settings.py
+++ pur_beurre_project/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["PUR_BEURRE_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+# if os.environ.get('ENV') == 'PRODUCTION':
+#     DEBUG = False
+# else:
+#     DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 

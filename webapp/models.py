f48fb0b9a1d64fb268b4b96a0cc151864937dd31
Awoodz <a.bois77@gmail.com>
2020-08-05 16:59:57 +0200

    PY add str method

--- webapp/models.py
+++ webapp/models.py
@@ -3,12 +3,18 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.category_name
+
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
     product_url = models.URLField(max_length=200, unique=True)
     product_img = models.URLField(max_length=200)
     product_nutriscore = models.CharField(max_length=10)
     product_category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name
     
 
 class Nutriments(models.Model):
@@ -29,4 +35,7 @@ class User(models.Model):
     user_mail = models.EmailField(max_length=100, unique=True)
     user_password = models.CharField(max_length=60)
     user_product = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.user_name
 

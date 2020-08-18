9b4626c00e015a440f91ec740745b0d34409b8b7
Awoodz <a.bois77@gmail.com>
2020-08-18 14:31:14 +0200

    test heroku

--- webapp/views.py
+++ webapp/views.py
@@ -22,6 +22,7 @@ def index(request):
     # return HttpResponse(message)
 
 
+# this is a test
 # def listing(request):
 #     products = Product.objects.filter()
 #     formatted_products = ["<li>{}</li>".format(product.product_name) for product in products]
